a34d26f64bf6a0c659e2e92bef5d68394130bf5d
eddwardmessias <eddward.messias@hotmail.com>
2022-05-12 07:49:21 -0300

    Alteração Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
+import {ReactComponent as GithubIcon} from './assets/img/github.svg';
+import Navbar from './components/Navbar';
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Navbar />
   );
 }
 
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import './styles.css';
+
+function Navbar(){
+
+    return (  <header>
+        <nav className="container">
+            <div className="dsmovie-nav-content">
+                <h1>Projeto Spring React </h1>
+                <a href="https://github.com/eddwardmessias" target="_blank" rel="noreferrer">
+                    <div className="dsmovie-contact-container">
+                        <GithubIcon />
+                        <p className="dsmovie-contact-link">/eddwardmessias</p>
+                    </div>
+                </a>
+            </div>
+        </nav>
+    </header>)
+
+}
+
+export default Navbar;
